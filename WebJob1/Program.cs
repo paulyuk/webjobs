5261ab9044f3e94eea8b7d3678762de2246c1827
Paul Yuknewicz <paulyuk@microsoft.com>
2024-04-26 13:23:34 -0700

    Add project files.

---
+++ WebJob1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob1
+{
+
+    internal class Program
+    {
+
+            static async Task Main()
+            {
+
+            var builder = new HostBuilder();
+                builder.ConfigureLogging((context, b) =>
+                {
+                    b.AddConsole();
+                });
+                builder.ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("appsettings.json");
+                });
+                builder.ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageQueues();
+                });
+
+
+
+            var host = builder.Build();
+                using (host)
+                {
+                    await host.RunAsync();
+                }
+            }
+       
+    }
+}
