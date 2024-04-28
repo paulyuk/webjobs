262531df9411d3ef20b7babde29883302f1d4b87
Paul Yuknewicz <paulyuk@microsoft.com>
2024-04-28 12:33:23 -0700

    experiment - try direct uris and creds

--- WebJob1/Program.cs
+++ WebJob1/Program.cs
@@ -8,6 +8,9 @@ using Azure.Core;
 using Azure.Identity;
 using Microsoft.Extensions.Azure;
 using System.Runtime.Remoting.Contexts;
+using Azure.Core.Extensions;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
 
 namespace WebJob1
 {
@@ -18,6 +21,9 @@ namespace WebJob1
         static async Task Main()
         {
             var credential = new DefaultAzureCredential();
+            // Create a BlobServiceClient using DefaultAzureCredential
+            BlobServiceClient blobServiceClient = new BlobServiceClient(new Uri("https://stwebjobfriday1.blob.core.windows.net/"), credential);
+            QueueServiceClient queueServiceClient = new QueueServiceClient(new Uri("https://stwebjobfriday1.queue.core.windows.net/"), credential);
 
             var builder = new HostBuilder();
                 builder.ConfigureLogging((context, b) =>
@@ -39,15 +45,30 @@ namespace WebJob1
                 builder.ConfigureServices(services =>
                 {
                     services.AddSingleton <TokenCredential> (credential);
-                    services.AddAzureClients(b =>
-                    {
-                        b.UseCredential(credential).AddBlobServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
 
-                        Console.WriteLine("Credential used: " + credential.ToString());
-                    });
+                    // Workaround, adding blob and queue clients directly with proper managed identity URI and credential; throws 'Microsoft.Azure.WebJobs.Host.Indexers.FunctionIndexingException -> ArgumentNullException: Value cannot be null.Parameter name: connectionString
+                    services.AddSingleton<BlobServiceClient>(blobServiceClient);
+                    services.AddSingleton<QueueServiceClient>(queueServiceClient);
+
+                    //// Standard way to add Azure clients for Blob and Queue; throws 'Microsoft.Azure.WebJobs.Host.Indexers.FunctionIndexingException -> ArgumentNullException: Value cannot be null.Parameter name: connectionString
+                    //services.AddAzureClients(b =>
+                    //{
+                    //    var blobServiceUri = Environment.GetEnvironmentVariable("AzureWebJobsStorage__blobServiceUri");
+                    //    var queueServiceUri = Environment.GetEnvironmentVariable("AzureWebJobsStorage__queueServiceUri");
+
+
+                    //    b.UseCredential(credential).AddBlobServiceClient(new Uri("https://stwebjobfriday1.blob.core.windows.net/"));
+                    //    b.UseCredential(credential).AddQueueServiceClient(new Uri("https://stwebjobfriday1.queue.core.windows.net/"));
+
+                    //    Console.WriteLine("Credential used: " + credential.ToString());
+                    //});
+
                 });
-                builder.ConfigureWebJobs(b =>
+
+
+            builder.ConfigureWebJobs(b =>
                 {
+                    b.AddAzureStorageBlobs();
                     b.AddAzureStorageQueues();
                 });
 
