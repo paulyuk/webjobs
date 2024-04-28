1e328b6c3fa971ead5b63e0cf701e6edec300b0a
Paul Yuknewicz <paulyuk@microsoft.com>
2024-04-28 15:35:19 -0700

    Use managed identity URI for queueclient

--- WebJob1/Program.cs
+++ WebJob1/Program.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Microsoft.Extensions.Azure;
-using System.Runtime.Remoting.Contexts;
-using Azure.Core.Extensions;
-using Azure.Storage.Blobs;
 using Azure.Storage.Queues;
 
 namespace WebJob1
@@ -20,63 +14,45 @@ namespace WebJob1
 
         static async Task Main()
         {
+            //uses Azure.Identity SDK 1.11.0 to match the version used in the WebJob extension template
             var credential = new DefaultAzureCredential();
-            // Create a BlobServiceClient using DefaultAzureCredential
-            BlobServiceClient blobServiceClient = new BlobServiceClient(new Uri("https://stwebjobfriday1.blob.core.windows.net/"), credential);
-            QueueServiceClient queueServiceClient = new QueueServiceClient(new Uri("https://stwebjobfriday1.queue.core.windows.net/"), credential);
+
+            // Create a QueueServiceClient using DefaultAzureCredential
+            // Uses Azure.Storage.Queues SDK 12.14.0 to match the version used in the WebJob extension template
+            var queueServiceClient = new QueueServiceClient(new Uri("https://stwebjobfriday1.queue.core.windows.net/"), credential);
 
             var builder = new HostBuilder();
-                builder.ConfigureLogging((context, b) =>
-                {
-                    // If the key exists in settings, use it to enable Application Insights.
-                    string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
-                    if (!string.IsNullOrEmpty(instrumentationKey))
-                    {
-                        b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
-                    }
 
-                    b.AddConsole();
-                    b.SetMinimumLevel(LogLevel.Information);
-                });
-                builder.ConfigureAppConfiguration(b =>
-                {
-                    b.AddJsonFile("appsettings.json");
-                });
                 builder.ConfigureServices(services =>
                 {
+                    // Test DefaultAzureCredential and ManagedIdentityCredential before adding to services
                     services.AddSingleton <TokenCredential> (credential);
+                    Console.WriteLine("Credential used: " + credential.ToString()); // should return "Azure.Identity.DefaultAzureCredential"
 
-                    // Workaround, adding blob and queue clients directly with proper managed identity URI and credential; throws 'Microsoft.Azure.WebJobs.Host.Indexers.FunctionIndexingException -> ArgumentNullException: Value cannot be null.Parameter name: connectionString
-                    services.AddSingleton<BlobServiceClient>(blobServiceClient);
                     services.AddSingleton<QueueServiceClient>(queueServiceClient);
 
-                    //// Standard way to add Azure clients for Blob and Queue; throws 'Microsoft.Azure.WebJobs.Host.Indexers.FunctionIndexingException -> ArgumentNullException: Value cannot be null.Parameter name: connectionString
-                    //services.AddAzureClients(b =>
-                    //{
-                    //    var blobServiceUri = Environment.GetEnvironmentVariable("AzureWebJobsStorage__blobServiceUri");
-                    //    var queueServiceUri = Environment.GetEnvironmentVariable("AzureWebJobsStorage__queueServiceUri");
-
-
-                    //    b.UseCredential(credential).AddBlobServiceClient(new Uri("https://stwebjobfriday1.blob.core.windows.net/"));
-                    //    b.UseCredential(credential).AddQueueServiceClient(new Uri("https://stwebjobfriday1.queue.core.windows.net/"));
-
-                    //    Console.WriteLine("Credential used: " + credential.ToString());
-                    //});
-
+                    // Test queueServiceClient before adding to services
+                    var q = queueServiceClient.GetQueues();
+                    foreach (var queue in q)
+                    {
+                        Console.WriteLine("Queue Test: Found queue with name'" + queue.Name + "'"); // should return "queue"
+                    }
                 });
 
 
-            builder.ConfigureWebJobs(b =>
+                builder.ConfigureWebJobs(b =>
                 {
-                    b.AddAzureStorageBlobs();
                     b.AddAzureStorageQueues();
                 });
-
-
 
             var host = builder.Build();
                 using (host)
                 {
+                    Console.WriteLine("Starting host..."); 
+
+                //throws exception here; Microsoft.Azure.WebJobs.Host.Indexers.FunctionIndexingException: 'Error indexing method 'Functions.ProcessQueueMessage'';
+                //ArgumentNullException: Value cannot be null.
+                //Parameter name: connectionString
                     await host.RunAsync();
                 }
         }
